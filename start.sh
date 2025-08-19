76b30599bdd1f84ed439df44180fd0bfada77dcd
sksender83-collab <sk.sender83@gmail.com>
2025-08-19 22:44:40 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python3 rss_server.py
+# Безкінечний цикл для автоматичного перезапуску скрипта у разі падіння
+while true; do
+    python3 rss_server.py
+    echo "Скрипт впав. Перезапуск через 5 секунд..."
+    sleep 5
+done
