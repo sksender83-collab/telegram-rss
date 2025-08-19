63edd5282c1e0c73e4d463e2eada9b665cd2a3b4
sksender83-collab <sk.sender83@gmail.com>
2025-08-19 21:29:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python3 rss_server.py
+
+# Безкінечний цикл для автоматичного перезапуску
+while true; do
+    echo "Запускаю rss_server.py..."
+    python3 rss_server.py
+    echo "rss_server.py завершився, перезапускаю через 5 секунд..."
+    sleep 5
+done
