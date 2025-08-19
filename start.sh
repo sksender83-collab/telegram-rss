479dad3f4c4fb2a3055058a7d6421943fc94d362
sksender83-collab <sk.sender83@gmail.com>
2025-08-19 23:01:09 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Безкінечний цикл для автоматичного перезапуску скрипта у разі падіння
-while true; do
-    python3 rss_server.py
-    echo "Скрипт впав. Перезапуск через 5 секунд..."
-    sleep 5
-done
+# Запуск FastAPI через uvicorn на Render
+uvicorn rss_server:app --host 0.0.0.0 --port $PORT
